b6af45da4cb63d27ad1fb9853157a7d6c962ca25
Barished <shedrackipi@gmail.com>
2023-04-25 21:49:22 +0000

    created 5_reversed_string.c

---
+++ 5_reversed_string.c
@@ -0,0 +1,85 @@
+#include "main.h"
+
+/**
+ * _memcpy - copy memory area
+ * @dest: Destination to copy to
+ * @src: Source to copy from
+ * @n: number of bytes to copy
+ * Return: The _memcpy() function returns a pointer to dest.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int q;
+
+	for (q = 0; q < n; i++)
+		dest[q] = src[q];
+
+	dest[q] = '\0';
+
+	return (dest);
+}
+
+
+/**
+ * rev_string - reverses a string
+ * @s: string to reverse
+ *
+ * Return: A pointer to a character
+ */
+
+char *rev_string(char *s)
+{
+	int lent;
+	int rev;
+	char tmp;
+	char *dest;
+
+	for (len = 0; s[lent] != '\0'; len++)
+	{}
+
+	dest = malloc(sizeof(char) * lent + 1);
+
+	if (dest == NULL)
+		return (NULL);
+
+	_memcpy(dest, s, len);
+	for (rev = 0; rev < lent; rev++, lent--)
+	{
+		tmp = dest[lent - 1];
+		dest[lent - 1] = dest[rev];
+		dest[rev] = tmp;
+	}
+
+	return (dest);
+}
+
+
+/**
+ * print_reversed - Calls a function to reverse and print a string
+ * @args: variable number of argument passed to the function
+ * Return: The amount of characters printed
+ */
+
+int print_reversed(va_list args)
+{
+	unsigned int lent;
+	char *str;
+	char *ptr;
+
+	str = va_arg(args, char *);
+
+	if (str == NULL)
+		return (-1);
+
+	ptr = rev_string(str);
+
+	if (ptr == NULL)
+		return (-1);
+
+	for (lent = 0; ptr[lent] != '\0'; lent++)
+		_write(ptr[lent]);
+
+	free(ptr);
+	return (lent);
+}
