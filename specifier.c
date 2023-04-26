630b61632c57f25a8a62c61259a8057dddbb2217
Samuel <ogboyesam@gmail.com>
2023-04-25 17:45:06 -0700

    It is well

--- specifier.c
+++ specifier.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+ * specifier - Select the appropriate function to handle a format specifier
+ * @c: The format specifier character
+ *
+ * Return: A function pointer to the appropriate conversion function
+ */
+
 int (*specifier(char c))(va_list)
 {
 	if (c == 'd' || c == 'i')
@@ -10,6 +17,16 @@ int (*specifier(char c))(va_list)
 		return (print_char);
 	else if (c == '%')
 		return (print_percent);
+	else if (c == 'b')
+		return (print_binary);
+	else if (c == 'u')
+		return (print_unint);
+	else if (c == 'o')
+		return (print_octal);
+	else if (c == 'r')
+		return (rot13);
+	else if (c == 'R')
+		return (print_reversed);
 	else
 		return (NULL);
 }
